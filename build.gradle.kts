c760644bb03159e2136a6b55ee0613867109325d
Steffen Grumbach <s.grumbach@intershop.de>
2022-09-23 11:24:13 +0200

    chore: Update 3rd party libs: org.slf4j:log4j-over-slf4j:1.7.36 & org.junit.jupiter:junit-jupiter:5.9.0 (#79406, #79347)

--- build.gradle.kts
+++ build.gradle.kts
@@ -120,10 +120,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.slf4j:log4j-over-slf4j:1.7.31")
+    implementation("org.slf4j:log4j-over-slf4j:1.7.36")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
