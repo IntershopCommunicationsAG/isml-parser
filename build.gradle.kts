ed631e03e24a3e1523a54a07b8ccd82e184d1776
Sven Kiesewetter <skiesewetter@intershop.com>
2025-08-15 08:37:19 +0200

    ci: migrate publishing to maven central (#106686)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitee.pkmer.enums.PublishingType
+
 plugins {
     // IDE plugin
     idea
@@ -11,6 +13,8 @@ plugins {
     signing
 
     id("com.intershop.gradle.javacc") version "5.0.1"
+
+    id("io.gitee.pkmer.pkmerboot-central-publisher") version "1.1.1"
 }
 
 group = "com.intershop.icm"
@@ -67,12 +71,15 @@ testing {
     }
 }
 
+val stagingRepoDir = project.layout.buildDirectory.dir("stagingRepo")
+
 publishing {
     publications {
         create("intershopMvn", MavenPublication::class.java) {
 
             from(components["java"])
-
+        }
+        withType<MavenPublication>().configureEach {
             pom {
                 name.set(project.name)
                 description.set(project.description)
@@ -115,14 +122,26 @@ publishing {
     }
     repositories {
         maven {
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
-            }
+            name = "LOCAL"
+            url = stagingRepoDir.get().asFile.toURI()
         }
+    }
+}
+
+pkmerBoot {
+    sonatypeMavenCentral{
+        // the same with publishing.repositories.maven.url in the configuration.
+        stagingRepository = stagingRepoDir
+
+        /**
+         * get username and password from
+         * <a href="https://central.sonatype.com/account"> central sonatype account</a>
+         */
+        username = sonatypeUsername
+        password = sonatypePassword
+
+        // Optional the publishingType default value is PublishingType.AUTOMATIC
+        publishingType = PublishingType.USER_MANAGED
     }
 }
 
